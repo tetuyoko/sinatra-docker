e3e768d9e1cb65be5e9b2453d7b44d390baf9666
tetuyoko <tyokoyama53@gmail.com>
2014-11-06 15:15:46 +0900

    add fig rails

---
+++ fig_rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = 'c9c91bf8d56842a83649e04841de3ccc8dea2c47ede65f15b167e964855bca64b6fee09ccc72df98cecc4da665e42e5863e38775e0e863506662fa6bd07492c3'
